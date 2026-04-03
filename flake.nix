c8a89063a175e280a75f16bf35ce87f8e33a8b02
CakZemprongzz <28718490+CakZemprongzz@users.noreply.github.com>
2026-04-03 08:43:08 +0700

    removal of lix and changes from bpfland to lavd

--- flake.nix
+++ flake.nix
@@ -18,26 +18,14 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    lix = {
-      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-      flake = false;
-    };
-
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.lix.follows = "lix";
-    };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nix-cachyos-kernel, aagl, home-manager, lix, lix-module, ... } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nix-cachyos-kernel, aagl, home-manager, ... } @ inputs:
   let
     makeConfig = { name, username, hostFile, extraModules ? [] }: nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [
-        lix-module.nixosModules.default
         aagl.nixosModules.default
         hostFile
         home-manager.nixosModules.home-manager {
--- modules/services.nix
+++ modules/services.nix
@@ -25,7 +25,7 @@
     };
     scx = {
       enable = true;
-      scheduler = "scx_bpfland";
+      scheduler = "scx_lavd";
     };
     udev.extraRules = builtins.readFile ./rules;
   };
